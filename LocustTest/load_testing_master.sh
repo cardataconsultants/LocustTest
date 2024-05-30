2455735c6838083449cf8af35ff385dd5c1dd2d0
FendEssa <efendish@cardata.co>
2024-05-30 11:33:05 -0400

    changing spawn rate

--- LocustTest/load_testing_master.sh
+++ LocustTest/load_testing_master.sh
@@ -1,2 +1,2 @@
 date=$(date +'%F_%H:%M:%S')
-locust --headless -f locustfile.py --master --users 400 --spawn-rate 3  -H https://api-staging.mi-route.com --html=./Reporting/report-locust-"$date".html
+locust --headless -f locustfile.py --master --users 400 --spawn-rate 2  -H https://api-staging.mi-route.com --html=./Reporting/report-locust-"$date".html
