80132b36b407fd7c1e8c96305794021173391ea7
FendEssa <efendish@cardata.co>
2024-05-30 14:42:20 -0400

    adding more users

--- LocustTest/load_testing_master.sh
+++ LocustTest/load_testing_master.sh
@@ -1,2 +1,2 @@
 date=$(date +'%F_%H:%M:%S')
-locust --headless -f locustfile.py --master --users 400 --spawn-rate 2  -H https://api-staging.mi-route.com --html=./Reporting/report-locust-"$date".html
+locust --headless -f locustfile.py --master --users 500 --spawn-rate 10  -H https://api-staging.mi-route.com --html=./Reporting/report-locust-"$date".html
