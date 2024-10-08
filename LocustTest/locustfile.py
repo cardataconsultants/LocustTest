5be9fd2d359d148dc90a38d7bfcf1a04c5a973ea
FendEssa <efendish@cardata.co>
2024-10-08 09:44:19 -0400

    adding map tests

--- LocustTest/locustfile.py
+++ LocustTest/locustfile.py
@@ -78,135 +78,135 @@ class User(HttpUser):
     today = (datetime.today()).strftime("%Y-%m-%d")
     url = os.environ['cloud_api_url']
 
-    @task(25)
+    @task
     def get_units(self):
         self.client.get("https://map-staging.cardataconsultants.com/api/getUnits/CA", headers=self.headers)
 
-    @task(25)
+    @task
     def get_distance(self):
         self.client.get("https://map-staging.cardataconsultants.com/api/getDistance?lat1=43.638779&lng1=-79.380653&lat2=43.873810&lng2=-78.963410&country=CA", headers=self.headers)
 
-    @task(25)
+    @task
     def get_geocode(self):
         self.client.get("https://map-staging.cardataconsultants.com/api/geocode?street=207", headers=self.headers)
 
-    @task(25)
+    @task
     def get_reverse_geocode(self):
         self.client.get("https://map-staging.cardataconsultants.com/api/reverseGeocode?latitude=43.638779&longitude=-79.380653", headers=self.headers)
 
-    @task(221)
-    def get_mileage_daily(self):
-        # print("GETTING MILEAGE FOR: " + str(self.user_id))
-        #print_now(str(self.user_id))
-        today = datetime.today().strftime("%Y-%m-%d")
-        self.client.get("/api/mileage/daily?date=" + today, headers=self.headers)
+    # @task(221)
+    # def get_mileage_daily(self):
+    #     # print("GETTING MILEAGE FOR: " + str(self.user_id))
+    #     #print_now(str(self.user_id))
+    #     today = datetime.today().strftime("%Y-%m-%d")
+    #     self.client.get("/api/mileage/daily?date=" + today, headers=self.headers)
+    #
+    # @task(40)
+    # def create_tracking_trip(self):
+    #     #print_now(str(self.user_id))
+    #
+    #     # print("CREATING TRACKING TRIP FOR: " + str(self.user_id))
+    #     trip = Helper.Helper.generate_tracking_trip_with_location(self.user_id, 43.638660, -79.387802)
+    #
+    #     response_json = (self.client.post("/api/v3/trip", json=trip, headers=self.headers)).json()
+    #     self.list_of_trips.append(response_json)
+    #
+    # @task(25)
+    # def get_user(self):
+    #     #print_now(str(self.user_id))
+    #
+    #     self.client.get("/api/v3/user", headers=self.headers)
+    #
+    # @task(43)
+    # def get_my_trips(self):
+    #     #print_now(str(self.user_id))
+    #
+    #     yesterday = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
+    #     today = (datetime.today()).strftime("%Y-%m-%d")
+    #     self.client.get(
+    #         "/api/v3/trips?trip_type=business;personal;unclassified&start_date=" + yesterday + "&end_date=" + today +
+    #         "&page=1",
+    #         headers=self.headers)
+    #
+    # @task(21)
+    # def get_company_saved_stops(self):
+    #     #print_now(str(self.user_id))
+    #
+    #     self.client.get(
+    #         "/api/company/" + str(self.company_id) + "/driver/" + str(
+    #             self.user_id) + "/trip_save_stops?date=" + self.today + "&short_list=1",
+    #         headers=self.headers, name='/api/company/{id}/{user_id}/trip_save_stops?date=2024-05-27&amp;short_list=1')
+    #
+    # @task(33)
+    # def get_mileage(self):
+    #     #print_now(str(self.user_id))
+    #
+    #     headers = self.headers
+    #     self.client.get("/api/mileage/", headers=headers)
+    #
+    # @task(7)
+    # def patch_tracking_trip(self):
+    #     #print_now(str(self.user_id))
+    #
+    #     headers = self.headers
+    #     trip = self.list_of_trips[0]
+    #     if trip is None:
+    #         return
+    #     trip_id = str(trip['id'])
+    #     for i in range(1, 4):
+    #         payload = {
+    #             "trip_id": trip_id,
+    #             "attribute": "classification",
+    #             "update": {
+    #                 "type_id": i
+    #             }
+    #         }
+    #         self.client.patch("/tracking/trip", json=payload, headers=headers)
+    #
+    # @task(25)
+    # def update_user_device(self):
+    #     #print_now(str(self.user_id))
+    #
+    #     iphone = {
+    #         "platform": "ios",
+    #         "version": "15.4.1",
+    #         "model": "iPhone13,2",
+    #         "manufacturer": "Apple",
+    #         "token": "<token>",
+    #         "app_version": "3.5.31",
+    #         "build_number": "116"
+    #     }
+    #
+    #     android = {
+    #         "platform": "android",
+    #         "version": "12.1",
+    #         "model": "SamsungS20",
+    #         "manufacturer": "Samsung",
+    #         "token": "<token>",
+    #         "app_version": "3.5.31",
+    #         "build_number": "117"
+    #     }
+    #     devices = [android, iphone]
+    #     for i in devices:
+    #         self.client.post("/api/user/" + str(self.user_id) + "/device/info", json=i, headers=self.headers, name='/api/user/{id}/device/info')
+    #
+    # @task(2)
+    # def create_manual_trip(self):
+    #     #print_now(str(self.user_id))
+    #
+    #     trip = Helper.Helper.generate_manual_trip("CA", "CA", self.user_id,
+    #                                        None, None,
+    #                                        None, None)
+    #
+    #     self.client.post("/api/v3/manualTrip", json=trip, headers=self.headers)
+    #
+    # @task(7)
+    # def get_user_schedule(self):
+    #     #print_now(str(self.user_id))
+    #
+    #     self.client.get("/api/user/" + str(self.user_id) + "/schedule", headers=self.headers, name='/api/user/{user_id}/schedule')
 
-    @task(40)
-    def create_tracking_trip(self):
-        #print_now(str(self.user_id))
-
-        # print("CREATING TRACKING TRIP FOR: " + str(self.user_id))
-        trip = Helper.Helper.generate_tracking_trip_with_location(self.user_id, 43.638660, -79.387802)
-
-        response_json = (self.client.post("/api/v3/trip", json=trip, headers=self.headers)).json()
-        self.list_of_trips.append(response_json)
-
-    @task(25)
-    def get_user(self):
-        #print_now(str(self.user_id))
-
-        self.client.get("/api/v3/user", headers=self.headers)
-
-    @task(43)
-    def get_my_trips(self):
-        #print_now(str(self.user_id))
-
-        yesterday = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
-        today = (datetime.today()).strftime("%Y-%m-%d")
-        self.client.get(
-            "/api/v3/trips?trip_type=business;personal;unclassified&start_date=" + yesterday + "&end_date=" + today +
-            "&page=1",
-            headers=self.headers)
-
-    @task(21)
-    def get_company_saved_stops(self):
-        #print_now(str(self.user_id))
-
-        self.client.get(
-            "/api/company/" + str(self.company_id) + "/driver/" + str(
-                self.user_id) + "/trip_save_stops?date=" + self.today + "&short_list=1",
-            headers=self.headers, name='/api/company/{id}/{user_id}/trip_save_stops?date=2024-05-27&amp;short_list=1')
-
-    @task(33)
-    def get_mileage(self):
-        #print_now(str(self.user_id))
-
-        headers = self.headers
-        self.client.get("/api/mileage/", headers=headers)
-
-    @task(7)
-    def patch_tracking_trip(self):
-        #print_now(str(self.user_id))
-
-        headers = self.headers
-        trip = self.list_of_trips[0]
-        if trip is None:
-            return
-        trip_id = str(trip['id'])
-        for i in range(1, 4):
-            payload = {
-                "trip_id": trip_id,
-                "attribute": "classification",
-                "update": {
-                    "type_id": i
-                }
-            }
-            self.client.patch("/tracking/trip", json=payload, headers=headers)
-
-    @task(25)
-    def update_user_device(self):
-        #print_now(str(self.user_id))
-
-        iphone = {
-            "platform": "ios",
-            "version": "15.4.1",
-            "model": "iPhone13,2",
-            "manufacturer": "Apple",
-            "token": "<token>",
-            "app_version": "3.5.31",
-            "build_number": "116"
-        }
-
-        android = {
-            "platform": "android",
-            "version": "12.1",
-            "model": "SamsungS20",
-            "manufacturer": "Samsung",
-            "token": "<token>",
-            "app_version": "3.5.31",
-            "build_number": "117"
-        }
-        devices = [android, iphone]
-        for i in devices:
-            self.client.post("/api/user/" + str(self.user_id) + "/device/info", json=i, headers=self.headers, name='/api/user/{id}/device/info')
-
-    @task(2)
-    def create_manual_trip(self):
-        #print_now(str(self.user_id))
-
-        trip = Helper.Helper.generate_manual_trip("CA", "CA", self.user_id,
-                                           None, None,
-                                           None, None)
-
-        self.client.post("/api/v3/manualTrip", json=trip, headers=self.headers)
-
-    @task(7)
-    def get_user_schedule(self):
-        #print_now(str(self.user_id))
-
-        self.client.get("/api/user/" + str(self.user_id) + "/schedule", headers=self.headers, name='/api/user/{user_id}/schedule')
-
-    @task(16)
+    @task
     def get_user_schedule_v4(self):
         #print_now(str(self.user_id))
 
