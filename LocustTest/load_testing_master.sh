62c2d2c91f6d3f1d810028feb6fd268dc24f3012
Juka <jukalope@gmail.com>
2024-10-09 20:56:28 -0400

    Fixed typo in load_testing_master.sh

--- LocustTest/load_testing_master.sh
+++ LocustTest/load_testing_master.sh
@@ -9,4 +9,4 @@ echo "    Ex. $0 500 0.1 10m"
 [ -z "$3" ] RUN_TIME=10m   || RUN_TIME=$3
 
 date=$(date +'%F_%H:%M:%S')
-locust --headless -f locustfile.py --master --users $USERS --spawn-rate $SPAWN_RATE --run-time=$10m -H https://map-staging.cardataconsultants.com --html=./Reporting/report-locust-"$date".html
+locust --headless -f locustfile.py --master --users $USERS --spawn-rate $SPAWN_RATE --run-time=$1 -H https://map-staging.cardataconsultants.com --html=./Reporting/report-locust-"$date".html
